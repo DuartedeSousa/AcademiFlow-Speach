49ee99dadaa5b9f38e6bc4d66992e0f0885c487d
Duarte <duarte.desousa.contato@gmail.com>
2025-11-19 12:20:12 -0300

    ServidorI

--- src/ServidorHttp.java
+++ src/ServidorHttp.java
@@ -6,13 +6,28 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class ServidorHttp {
+
+
+
     public static void main(String[] args) throws Exception {
+
+        final String[] usuarioCadastro = new String[1];
+        final String[] senhaCadastro = new String[1];
+        final String[] cargoCadastro = new String[1];
+
         // Cria servidor HTTP escutando na porta 8080
         HttpServer servidor = HttpServer.create(new InetSocketAddress(8081), 0);
+
+
 
         /* INICIO DO CÓDIGO */
         servidor.createContext("/", troca->{
             enviarArquivo(troca, "index.html", "text/html");
+        });
+
+
+        servidor.createContext("/cadastro", troca->{
+            enviarArquivo(troca, "cadastro.html", "text/html");
         });
 
         servidor.createContext("/login", troca->{
@@ -23,27 +38,26 @@ public class ServidorHttp {
 
             String usuario;
             String senha;
-            String perfil;
+            String cargo;
 
 
             usuario = partes[0].replace("usuario=", "");
             senha = partes[1].replace("senha=", "");
-            perfil = partes[2].replace("perfil=", "");
-            perfil = perfil.toLowerCase();
+            cargo = partes[2].replace("cargo=", "");
 
-            if(usuario.equals("Duarte") && senha.equals("123")){
+            if(usuario.equals("cadastroA") && senha.equals("cadastroB")){
                 System.out.println("Acesso autorizado");
                 System.out.println(usuario);
                 System.out.println(senha);
-                System.out.println(perfil);
+                System.out.println(cargo);
 
-                if(perfil.equals("comum")){
-                    troca.getResponseHeaders().set("Location","/comum");
+                if(cargo.equals("professor")){
+                    troca.getResponseHeaders().set("Location","/sobre");
                     troca.sendResponseHeaders(302, -1);
                 }
 
-                else if(perfil.equals("adm")){
-                    troca.getResponseHeaders().set("Location","/adm");
+                else if(cargo.equals("aluno")){
+                    troca.getResponseHeaders().set("Location","/sobre");
                     troca.sendResponseHeaders(302, -1);
                 }
             }
@@ -57,13 +71,27 @@ public class ServidorHttp {
 
 
 
-        servidor.createContext("/comum", troca->{
-            enviarArquivo(troca, "comum.html", "text/html");
+
+
+
+
+        servidor.createContext("/criarcadastro", troca->{
+            String query = troca.getRequestURI().getQuery();
+
+            String[] partes;
+            partes = query.split("&");
+
+
+
+            usuarioCadastro[0] = partes[0].replace("usuario=", "");
+            senhaCadastro[0] = partes[1].replace("senha=", "");
+            cargoCadastro[0] = partes[2].replace("cargo=", "");
+
         });
 
-        servidor.createContext("/adm", troca->{
-            enviarArquivo(troca, "adm.html", "text/html");
-        });
+
+
+
 
         servidor.createContext("/estilo.css", troca->{
             enviarArquivo(troca, "estilo.css", "text/css");
