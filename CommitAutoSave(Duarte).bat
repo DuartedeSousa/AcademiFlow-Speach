bcc0d20139909f9325b8d4ebef93f6e2444f9a18
Duarte <duarte.desousa.contato@gmail.com>
2025-11-27 20:31:57 -0300

    correcaoDeProblemas

--- CommitAutoSave(Duarte).bat
+++ CommitAutoSave(Duarte).bat
@@ -3,4 +3,5 @@ git config --global user.email duarte.desousa.contato@gmail.com
 git add .
 git commit -m "correcaoDeProblemas"
 git branch -M main
+git remote add origin https://github.com/DuartedeSousa/AcademiFlow-Speach.git
 git push -u origin main
