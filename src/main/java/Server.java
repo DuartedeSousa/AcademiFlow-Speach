b2b313bddde505dbcd034eb11713764c3f079480
Duarte <duarte.desousa.contato@gmail.com>
2025-12-01 16:22:31 -0300

    testes

---
+++ src/main/java/Server.java
@@ -0,0 +1,19 @@
+package main.java;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+
+public class Server {
+
+    private static Connection con;
+
+    public static void main(String[] args) throws Exception{
+
+
+    }
+}
