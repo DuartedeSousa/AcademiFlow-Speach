5a6499d3a9d1854d08e28c7ecad7b214b13fad48
Duarte <duarte.desousa.contato@gmail.com>
2025-12-06 15:49:24 -0300

    botaoDeEditar

--- CommitAutoSave(Duarte).bat
+++ CommitAutoSave(Duarte).bat
@@ -3,5 +3,4 @@ git config --global user.email duarte.desousa.contato@gmail.com
 git add .
 git commit -m "botaoDeEditar"
 git branch -M main
-git remote add origin https://github.com/DuartedeSousa/AcademiFlow-Speach.git
 git push -u origin main
